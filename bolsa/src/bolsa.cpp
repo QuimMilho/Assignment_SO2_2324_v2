725c28ca48c8a3e9c5e975e1522b7f6a7792358f
Joaquim Milheiro <jrodmilheiro@gmail.com>
2024-05-14 18:03:26 +0100

    Criada função main do programa bolsa

--- bolsa/src/bolsa.cpp
+++ bolsa/src/bolsa.cpp
@@ -1,5 +1,15 @@
 #include "pch/so2pch.h"
 
+#include "dynamicLib.h"
+#include "ServerData/ServerData.h"
+#include "ui/AppUI.h"
+
 int _tmain(int argc, TCHAR** argv) {
+	DEFINE_UNICODE;
+
+	AppContext app;
+	AppUI appUI(app);
+	appUI.start();
+
 	return 0;
 }
