09abb0be57280d6845000abb0a7fb842adc4a406
Joaquim Milheiro <jrodmilheiro@gmail.com>
2024-05-17 12:48:31 +0100

    Adicionada estrutura da empresa que é partilhada na memória dinâmica.

--- dynamicLib/src/Data/Data.h
+++ dynamicLib/src/Data/Data.h
@@ -3,7 +3,17 @@
 #include "macros.h"
 #include "pch/so2pch.h"
 
-struct SO2_API SharedCompany {
-	TCHAR nome[32];
+#define BOARD_SHARED_MEMORY _T("BoardSharedMemory")
+
+#define BOARD_UPDATE_EVENT _T("BoardUpdateEvent")
+#define APP_CLOSE_EVENT _T("AppCloseEvent")
+
+struct SharedCompany {
+	TCHAR nome[MAX_STRING];
 	double share_price, total_value;
+};
+
+struct BoardData {
+	SharedCompany companies[10];
+	int nCompanies;
 };
