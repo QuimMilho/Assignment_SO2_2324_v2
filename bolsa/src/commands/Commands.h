f22cd5d9a46650c7eece14fa3316c83909c5d20d
Joaquim Milheiro <jrodmilheiro@gmail.com>
2024-05-17 14:09:50 +0100

    fix header de comandos

--- bolsa/src/commands/Commands.h
+++ bolsa/src/commands/Commands.h
@@ -1,15 +1,15 @@
 #pragma once
 
-#include "clientes/CmdAddC.h"
-#include "clientes/CmdClose.h"
-#include "clientes/CmdListC.h"
-#include "clientes/CmdPause.h"
-#include "clientes/CmdStock.h"
-#include "clientes/CmdUsers.h"
-
-#include "bolsa/CmdBalance.h"
-#include "bolsa/CmdBuy.h"
-#include "bolsa/CmdExit.h"
+#include "bolsa/CmdAddC.h"
+#include "bolsa/CmdClose.h"
 #include "bolsa/CmdListC.h"
-#include "bolsa/CmdLogin.h"
-#include "bolsa/CmdSell.h"
+#include "bolsa/CmdPause.h"
+#include "bolsa/CmdStock.h"
+#include "bolsa/CmdUsers.h"
+
+#include "clientes/CmdBalance.h"
+#include "clientes/CmdBuy.h"
+#include "clientes/CmdExit.h"
+#include "clientes/CmdListC.h"
+#include "clientes/CmdLogin.h"
+#include "clientes/CmdSell.h"
