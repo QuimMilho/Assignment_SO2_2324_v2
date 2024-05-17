c93b893676585a9ddba6f7ed52cc1c720788143e
Joaquim Milheiro <jrodmilheiro@gmail.com>
2024-05-17 12:42:17 +0100

    Implementado comando addc e listc

--- bolsa/src/commands/clientes/CmdAddC.h
+++ bolsa/src/commands/clientes/CmdAddC.h
@@ -5,6 +5,50 @@
 struct CmdAddC : public Command {
 protected:
 	int function(std::vector<_TSTRING>& args, AppContext& app) {
+		if (args.size() == 2) {
+			int r = app.addCompanies(args.at(1));
+			if (r == -1) {
+				_TCOUT << _T("A bolsa está atualmente em pausa! Nenhuma empresa foi adicionada!") << _TENDL;
+				return CMD_OK;
+			} else if (r == -2) {
+				_TCOUT << _T("O ficheiro não existe!") << _TENDL;
+				return CMD_INVALID_ARGS;
+			} else if (r) {
+				_TCOUT << _T("Foram lidas ") << r << _T(" empresas a partir do ficheiro ") << args.at(1) 
+						<< _TENDL;
+				return CMD_OK;
+			} else {
+				_TCOUT << _T("Nenhuma empresa foi lida a partir do ficheiro ") << args.at(1) << _TENDL;
+				return CMD_OK;
+			}
+		} else if (args.size() == 4) {
+			try {
+				int total_shares = std::stoi(args.at(2));
+				double share_price = std::stod(args.at(3));
+				int r = app.addCompanies(args.at(1), total_shares, share_price);
+				if (r == -1) {
+					_TCOUT << _T("A bolsa está atualmente em pausa! Nenhuma empresa foi adicionada!") << _TENDL;
+					return CMD_OK;
+				} else if (r) {
+					_TCOUT << _T("A empresa ") << args.at(1) << _T(" foi adicionada com sucesso!") << _TENDL;
+					return CMD_OK;
+				} else {
+					LOG_ERRO(_T("A empresa não foi adicionada. Erro: %d - N:%d"), r, app.getApp().nCompanies());
+					_TCOUT << _T("A empresa ") << args.at(1) << _T(" não foi adicionada!") << _TENDL;
+					return CMD_INVALID_ARGS;
+				}
+			} catch (std::invalid_argument& e) {
+				_TCOUT << _T("Algum dos argumentos introduzidos é inválido!") << _TENDL;
+				return CMD_INVALID_ARGS;
+			} catch (std::out_of_range& e) {
+				_TCOUT << _T("Algum dos argumentos introduzidos está fora do intervalo perimitido!") << _TENDL;
+				return CMD_INVALID_ARGS;
+			}
+		} else {
+			_TCOUT << _T("O comando foi mal utilizado!\n") << _T("Deves usar \"addc <nomeficheiro>\" ou\n") 
+					<< _T("\"addc <nome empresa> <total de ações> <preço da ação>\"") << _TENDL;
+			return CMD_INVALID_ARGS;
+		}
 		return CMD_OK;
 	}
 };
--- bolsa/src/commands/clientes/CmdListC.h
+++ bolsa/src/commands/clientes/CmdListC.h
@@ -5,6 +5,17 @@
 struct CmdListC : public Command {
 protected:
 	int function(std::vector<_TSTRING>& args, AppContext& app) {
+		int n = app.getApp().nCompanies();
+		if (n == 0) {
+			_TCOUT << _T("Não há empresas registadas no sistema!") << _TENDL;
+			return CMD_OK;
+		}
+		_TCOUT << _T("Há ") << n << _T(" empresas registadas:") << _TENDL;
+		Company* comps = app.getApp().getCompanies();
+		for (int i = 0; i < n; i++) {
+			Company& c = comps[i];
+			_TCOUT << c << _TENDL;
+		}
 		return CMD_OK;
 	}
 };
