ac1b921269f4ffca4e28da9bbc60e63b7bffc78f
Joaquim Milheiro <jrodmilheiro@gmail.com>
2024-05-17 12:46:05 +0100

    Adicionado getter e operadores de print e de copia para SharedCompany

--- bolsa/src/ServerData/Company/Company.cpp
+++ bolsa/src/ServerData/Company/Company.cpp
@@ -9,3 +9,21 @@ Company::Company(const _TSTRING& name, const int n_shares, const double share_pr
 _TSTRING Company::getName() const {
 	return name;
 }
+
+double Company::getSharedPrice() const {
+	return share_price;
+}
+
+Company& operator>>(Company& c, SharedCompany& sc) {
+	_TSTRCPY_S(sc.nome, MAX_STRING, c.name.c_str());
+	sc.share_price = c.share_price;
+	sc.total_value = c.total_value;
+	return c;
+}
+
+_TOSTREAM& operator<<(_TOSTREAM& o, Company& c) {
+	TCHAR str[512];
+	_TSPRINTF(str, _T("Nome: %32s - Total de ações: %6d - Preço das ações: %.3f - Valor total: %.2f"),
+			c.name.c_str(), c.total_shares, c.share_price, c.total_value);
+	return o << str;
+}
--- bolsa/src/ServerData/Company/Company.h
+++ bolsa/src/ServerData/Company/Company.h
@@ -8,6 +8,10 @@ struct Company {
 	Company(const _TSTRING& name, const int n_shares, const double share_price);
 
 	[[nodiscard]] _TSTRING getName() const;
+	[[nodiscard]] double getSharedPrice() const;
+
+	friend Company& operator >> (Company& c, SharedCompany& sc);
+	friend _TOSTREAM& operator << (_TOSTREAM& o, Company& c);
 
 private:
 	_TSTRING name;
