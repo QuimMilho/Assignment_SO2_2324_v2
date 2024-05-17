c00e3f8aa9cdea9ecfeac9e9dceda757f10e6463
Joaquim Milheiro <jrodmilheiro@gmail.com>
2024-05-17 16:22:09 +0100

    Adicionado mutex para ver se a aplicação já está a correr!

--- bolsa/src/bolsa.cpp
+++ bolsa/src/bolsa.cpp
@@ -4,12 +4,22 @@
 #include "ServerData/ServerData.h"
 #include "ui/AppUI.h"
 
+#define APP_RUNNING_MUTEX _T("AppRunningMutex")
+
 int _tmain(int argc, TCHAR** argv) {
 	DEFINE_UNICODE;
+
+	WindowsMutex mutex(APP_RUNNING_MUTEX);
+	if (mutex.wait(0) != WAIT_OBJECT_0) {
+		_TCOUT << _T("Já existe um programa bolsa em execução!") << _TENDL;
+		return -1;
+	}
 
 	AppContext app;
 	AppUI appUI(app);
 	appUI.start();
+
+	mutex.release();
 
 	return 0;
 }
