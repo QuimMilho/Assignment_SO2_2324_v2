563b540d7c19e6d160600870114b37ca6119c938
Joaquim Milheiro <jrodmilheiro@gmail.com>
2024-05-17 12:53:26 +0100

    Adicionadas novas Handles ao ficheiro .h

--- dynamicLib/src/Windows/WindowsHandles.h
+++ dynamicLib/src/Windows/WindowsHandles.h
@@ -2,5 +2,9 @@
 
 #include "WindowsHandle.h"
 #include "WindowsRegistryKey.h"
+#include "WindowsTimer.h"
 #include "handles/WindowsMutex.h"
 #include "handles/WindowsThread.h"
+#include "handles/WindowsNamedPipe.h"
+#include "handles/WindowsSharedMemory.h"
+#include "handles/WindowsEvent.h"
