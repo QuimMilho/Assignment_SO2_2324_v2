d5d5224c9a6b3c62554f8ed095bb656ba0706a05
Joaquim Milheiro <jrodmilheiro@gmail.com>
2024-05-17 14:07:37 +0100

    Comandos no sítio errado xD

--- bolsa/src/commands/bolsa/CmdListC.h
+++ bolsa/src/commands/bolsa/CmdListC.h
@@ -2,9 +2,20 @@
 
 #include "commands/Command.h"
 
-struct CmdCListC : public Command {
+struct CmdListC : public Command {
 protected:
 	int function(std::vector<_TSTRING>& args, AppContext& app) {
+		int n = app.getApp().nCompanies();
+		if (n == 0) {
+			_TCOUT << _T("Não há empresas registadas no sistema!") << _TENDL;
+			return CMD_OK;
+		}
+		_TCOUT << _T("Há ") << n << _T(" empresas registadas:") << _TENDL;
+		Company* comps = app.getApp().getCompanies();
+		for (int i = 0; i < n; i++) {
+			Company& c = comps[i];
+			_TCOUT << c << _TENDL;
+		}
 		return CMD_OK;
 	}
 };
